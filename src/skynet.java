39762109943f0c2e5974d6081056fc7a4900cbdb
RahulHirlekar <rahulhirlekar798@gmail.com>
2023-08-02 13:53:40 +0530

    intial demo

---
+++ src/skynet.java
@@ -0,0 +1,46 @@
+public class skynet {
+
+            int a = 10 ;
+           int b = 15 ;
+           int c = 20 ;
+            int d = 25 ;
+
+        public  void sum () {
+            int e = a + b;
+
+            System.out.print("sum = " + e);
+                }
+
+
+            public void sub () {
+
+                int f = b - c;
+                System.out.print("sub = " + f);
+
+            }
+
+            public void div () {
+
+                int g = c / d;
+                System.out.print("div = " + g);
+                 }
+
+
+
+                public void mul () {
+                    int h = d * a;
+                    System.out.print("mul = " + h);
+
+                }
+
+                    public  void  main(String[] args) {
+
+                                   mul();
+                                   div();
+                                   sub();
+                                    sum();
+
+                    }
+
+                  }
+
